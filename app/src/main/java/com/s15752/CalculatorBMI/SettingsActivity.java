2629758eb45a70cba42cf323fe85742f49de51a5
Karol Kwiatkowski <kwiatkowski.karol.88@gmail.com>
2020-04-02 00:22:47 +0200

    add missing project files

---
+++ app/src/main/java/com/s15752/CalculatorBMI/SettingsActivity.java
@@ -0,0 +1,38 @@
+// SettingsActivity.java
+// Activity to display SettingsActivityFragment on a phone
+package com.s15752.CalculatorBMI;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toolbar;
+
+import com.learn2crack.activities.R;
+
+public class SettingsActivity extends Activity {
+   // inflates the GUI, displays Toolbar and adds "up" button
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_settings);
+      Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      setSupportActionBar(toolbar);
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+   }
+}
+
+/*************************************************************************
+ * (C) Copyright 1992-2016 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
